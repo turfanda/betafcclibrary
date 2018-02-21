0c25990f0f801b0663aa4baad893c79372910f51
Glitch (betafcc-personellibraray) <none>
2018-02-21 07:12:42 +0000

    🎡🏎 Checkpoint test/test.js:596315/333

--- test/test.js
+++ test/test.js
@@ -23,21 +23,23 @@ describe("Tests", function() {
                     expect(res.body).to.have.property('id');
                     assert.isString(res.body.book_title);
                     assert.deepEqual(res.body.book_title, book.book_title);  
-                    book.id = res.body._id;  
+                    book.id = res.body.id;  
                     done();
                 });
         });
         it("AddComment", function(done) {
-            chai.request(app)
+          chai.request(app)
                 .post('/api/books/'+book.id)
                 .send({book_comment: book.book_comment})
                 .end(function(err, res) {
+            console.log(res.body);
                     expect(res.body).to.have.property('book_title');
-                    expect(res.body).to.have.property('id');
+                    expect(res.body).to.have.property('_id');
+                    expect(res.body).to.have.property('book_comment');
                     assert.isString(res.body.book_title);
-                    assert.deepEqual(res.body.book_title, book.book_title);  
-                    book.id = res.body._id;  
-                    done();
+                    assert.deepEqual(res.body.book_title, book.book_title);                      
+                    assert.isArray(res.body.book_comment);
+                    done();   
                 });
         });
       }
