11dfcbed340f51ea4f567bfcc172ab1f0abeebce
Glitch (betafcc-personellibraray) <none>
2018-02-21 07:01:41 +0000

    🐀🕹 Checkpoint test/test.js:596315/425

--- test/test.js
+++ test/test.js
@@ -1,27 +1,45 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const assert = chai.assert;
+const expect = chai.expect;
 const mocha = require('mocha')
 const describe = mocha.describe
 const it = mocha.it
 let app = require("../server");
 chai.use(chaiHttp);
 
+let books = [{book_title: 'book1', book_comment: 'comment1', id: ''},
+             {book_title: 'book2', book_comment: 'comment2', id: ''}]
+
 describe("Tests", function() {
     describe("Functionla Test", function() {
-        it("createProject", function(done) {
+      for (let book of books){
+        it("createBook", function(done) {
             chai.request(app)
-                .post('/api')
-                .send({
-                    project_name: "pr10"
-                })
+                .post('/api/books')
+                .send({book_title:book.book_title})
                 .end(function(err, res) {
-                    assert.equal(res.status, 200, 'response status should be 200');
-                    assert.equal(res.type, 'text/html', "Response should be json");
-                    assert.include(res.text, 'pr10', 'string contains substring');
-
+                    expect(res.body).to.have.property('book_title');
+                    expect(res.body).to.have.property('id');
+                    assert.isString(res.body.book_title);
+                    assert.deepEqual(res.body.book_title, book.book_title);  
+                    book.id = res.body._id;  
                     done();
                 });
         });
+        it("AddComment", function(done) {
+            chai.request(app)
+                .post('/api/books/'+book.id)
+                .send({book_comment: book.book_comment})
+                .end(function(err, res) {
+                    expect(res.body).to.have.property('book_title');
+                    expect(res.body).to.have.property('id');
+                    assert.isString(res.body.book_title);
+                    assert.deepEqual(res.body.book_title, book.book_title);  
+                    book.id = res.body._id;  
+                    done();
+                });
+        });
+      }
     });
 });
